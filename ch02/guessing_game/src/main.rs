e7ba60f2cea5becc067343b7403c682a898f3fd9
pretzel-wav <justin.macak@gmail.com>
2025-01-27 12:49:04 -0500

    listing 2-6

--- ch02/guessing_game/src/main.rs
+++ ch02/guessing_game/src/main.rs
@@ -7,8 +7,6 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1,101); // inclusive on lower bound, exclusive on upper bound
 
-    println!("The secret number is: {}", secret_number);
-
     loop {
         println!("Please input your guess.");
 
@@ -20,7 +18,7 @@ fn main() {
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
-        }
+        };
 
         println!("You guessed: {}", guess);
 
