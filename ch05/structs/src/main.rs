c779209820b31666c327733e232694e307745759
pretzel-wav <justin.macak@gmail.com>
2025-01-30 10:28:33 -0500

    listing 5-2

--- ch05/structs/src/main.rs
+++ ch05/structs/src/main.rs
@@ -6,5 +6,10 @@ struct User {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
 }
